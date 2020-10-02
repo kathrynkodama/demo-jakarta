8495dd66653eff258c02969ef1f40b88f57dae67
Ryan Zegray <ryan.zegray@ibm.com>
2020-10-02 14:52:39 -0400

    stuff

---
+++ src/main/java/io/openliberty/sample/system/SimpleHello.java
@@ -0,0 +1,20 @@
+package io.openliberty.sample.system;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "hello", urlPatterns = { "/hello" })
+public class SimpleHello extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html;charset=UTF-8");
+
+        resp.getWriter().println("Hello Jakarta EE 9!");
+    }
+}
