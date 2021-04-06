6c601decbd19fce546f2c72cf8c08e957f66b982
Kathryn Kodama <kathryn.s.kodama@gmail.com>
2021-04-06 17:05:33 -0400

    added post method for jax-rs example
    Signed-off-by: Kathryn Kodama <kathryn.s.kodama@gmail.com>

--- src/main/java/io/openliberty/sample/jakarta/beanval/Person.java
+++ src/main/java/io/openliberty/sample/jakarta/beanval/Person.java
@@ -1,41 +1,43 @@
 package io.openliberty.sample.jakarta.beanval;
 
-import jakarta.persistence.Entity;
 import jakarta.validation.constraints.AssertFalse;
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Positive;
 
-@Entity
-public class PersonEntity {
+public class Person {
 	
-//	@Positive
+	@Positive
+    private Integer studentId;
+	
+	@NotNull
 	private String name;
 
-//    @Email
+    @Positive
     private int age;
 
 //    @AssertTrue
 //    private boolean isSubscribed;
 //    
-    @AssertTrue
-    public boolean isHuman() {
-        return true;
-    }
+//    @AssertTrue
+//    public boolean isHuman() {
+//        return true;
+//    }
     
 //    @AssertFalse
 //    public static boolean isCat() {
 //        return false;
 //    }
     
-    public PersonEntity() {
-    	// this constructor has args!
+    public String getName() {
+    	return this.name;
     }
     
-    public PersonEntity(String name) {
-    	this.name = name; 
+    public Person(String name) {
+    	this.name = name;
     }
 }
