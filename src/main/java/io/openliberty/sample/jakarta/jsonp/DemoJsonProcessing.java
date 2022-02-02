46147ce15585391baece30772d65406b351db086
Adit Rada <rada_adit@hotmail.com>
2022-02-01 20:16:04 -0700

    Added json pointer example - not working as unsopported

--- src/main/java/io/openliberty/sample/jakarta/jsonp/DemoJsonProcessing.java
+++ src/main/java/io/openliberty/sample/jakarta/jsonp/DemoJsonProcessing.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
@@ -22,6 +23,7 @@ public class DemoJsonProcessing {
 	public static void main(String[] args) {
 		writeJson(readJson("src/main/java/io/openliberty/sample/jakarta/jsonp/data.json"));
 		writeJson(buildJson());
+		// jsonPointer();
 	}
 	
 	public static JsonObject readJson(String filepath) {
@@ -64,4 +66,23 @@ public class DemoJsonProcessing {
 				.build();
 		return jsonObject;
 	}
+
+	// public static void jsonPointer() {
+	// 	String jsonString = "{\"name\":\"duke\",\"age\":42,\"skills\":[\"Java SE\", \"Java EE\"]}";
+	
+	// 	JsonObject jsonObject = Json.createReader(new StringReader(jsonString)).readObject();
+	
+	// 	JsonPointer arrayElementPointer = Json.createPointer("/skills/1");
+	// 	JsonPointer agePointer = Json.createPointer("/age");
+	// 	JsonPointer namePointer = Json.createPointer("/name");
+	// 	JsonPointer addressPointer = Json.createPointer("/address");
+	// 	JsonPointer tagsPointer = Json.createPointer("/tags");
+	
+	// 	System.out.println("Get array element with pointer: " + arrayElementPointer.getValue(jsonObject).toString());
+	// 	System.out.println("Remove age with pointer: " + agePointer.remove(jsonObject));
+	// 	System.out.println("Replace name with pointer: " + namePointer.replace(jsonObject, Json.createValue("john")));
+	// 	System.out.println("Check address with pointer: " + addressPointer.containsValue(jsonObject));
+	// 	System.out.println("Add tags with pointer: " + tagsPointer.add(jsonObject, Json.createArrayBuilder().add("nice").build()));
+	//   }
+
 }
